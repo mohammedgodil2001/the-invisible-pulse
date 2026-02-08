9b095115027a99aec0b75f2469f8716f3524094f
mohammedgodil <mohammed.godil@student.howest.be>
2026-02-08 12:54:47 +0100

    final touches and deleting comments before submitting

--- plantCode.ino
+++ plantCode.ino
@@ -437,21 +437,17 @@ int scaleSearch(int note, int scale[], int scalesize) {
       if (note < scale[i]) {
         return scale[i];
       }
-    } // highest scale value less than or equal to note
-    // otherwise continue search
+    } 
+    
   }
-  // didn't find note and didn't pass note value, uh oh!
-  return 6; // give arbitrary value rather than fail
+  return 6; 
 }
 
 int scaleNote(int note, int scale[], int root) {
-  // input note mod 12 for scaling, note/12 octave
-  // search array for nearest note, return scaled*octave
   int scaled = note % 12;
   int octave = note / 12;
   int scalesize = (scale[0]);
-  // search entire array and return closest scaled note
   scaled = scaleSearch(scaled, scale, scalesize);
-  scaled = (scaled + (12 * octave)) + root; // apply octave and root
+  scaled = (scaled + (12 * octave)) + root; 
   return scaled;
 }
